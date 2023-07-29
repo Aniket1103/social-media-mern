07b4da4e4f7ba72b5612ec5a34b5a97b184538f9
Aniket1103 <55594740+Aniket1103@users.noreply.github.com>
2023-07-30 01:45:34 +0530

    issue - handled OTP expiry issue on production, temporary soln

--- backend/controllers/user.js
+++ backend/controllers/user.js
@@ -22,7 +22,7 @@ export const register = async (req, res) => {
       email,
       password,
       otp,
-      otp_expiry: new Date(Date.now() + process.env.OTP_EXPIRE * 60 * 1000),
+      // otp_expiry: new Date(Date.now() + process.env.OTP_EXPIRE * 60 * 1000),
     })
 
     return res.send(user);
